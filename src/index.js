1503afac863486a14a3656ea81504c53b44fd348
Sheldon <chi.sheldon@gmail.com>
2016-10-17 22:05:12 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-// TODO: unset
-
 const methodsMapping = {
-	get: 'GET',
-	post: 'POST',
-	put: 'PUT',
-	del: 'DELETE',
-	patch: 'PATCH'
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  del: 'DELETE',
+  patch: 'PATCH'
 };
 
 const mock = {};
@@ -20,120 +18,123 @@ let routesMap = {};
 let currentRoute;
 
 function buildRoute(method, url, reply) {
-	return {
-		method,
-		url: baseUrl + url,
-		reply
-	};
+  return {
+    method,
+    url: baseUrl + url,
+    reply
+  };
 }
 
 function buildRouteKey(httpMethod, url) {
-	return httpMethod + ' ' + url;
+  return httpMethod + ' ' + url;
 }
 
 function addRoute(method, url) {
-	const route = buildRoute(method, url);
-	const routeKey = buildRouteKey(route.method, route.url);
-	routesMap[routeKey] = route;
-	currentRoute = route;
-	return mock; // chaining
+  const route = buildRoute(method, url);
+  const routeKey = buildRouteKey(route.method, route.url);
+  routesMap[routeKey] = route;
+  currentRoute = route;
+  return mock; // chaining
 }
 
 // TODO: mock.delay;
 
 // Sugar methods to add routes by http methods
 Object.keys(methodsMapping).forEach(method => {
-	const httpMethod = methodsMapping[method];
-	mock[method] = addRoute.bind(null, httpMethod);
+  const httpMethod = methodsMapping[method];
+  mock[method] = addRoute.bind(null, httpMethod);
 });
 
 // Reply function
 mock.reply = (status, result) => {
-	if (!currentRoute) {
-		throw new Error('Must call get, post, put, del or patch before reply');
-	}
+  if (!currentRoute) {
+    throw new Error('Must call get, post, put, del or patch before reply');
+  }
 
-	currentRoute.reply = {
-		status,
-		result
-	};
+  currentRoute.reply = {
+    status,
+    result
+  };
 
-	return mock; // chaining
+  return mock; // chaining
 };
 
 mock.clear = () => {
-	routesMap = {};
-	return mock; // chaining
+  routesMap = {};
+  return mock; // chaining
 };
 
 function init(requestBaseUrl = '') {
-	baseUrl = requestBaseUrl; // Set the baseUrl
-	return mock;
+  baseUrl = requestBaseUrl; // Set the baseUrl
+  return mock;
 }
 
 export default function(superagent) {
-	// don't patch if superagent was patched already
-	if (superagent._patchedBySuperagentMocker) {
-		return init;
-	}
 
-	superagent._patchedBySuperagentMocker = true;
+  // don't patch if superagent was patched already
+  if (superagent._patchedBySuperagentMocker) {
+    return init;
+    mock.clear() // sheldon: added reset here
+  }
 
-	// Patch the end method to shallow the request and return the result
-	const reqProto = superagent.Request.prototype;
-	const oldEnd = reqProto.end;
-	reqProto.end = function end(cb) { // do not use function arrow to access to this.url and this.method
-		const route = routesMap[buildRouteKey(this.method, this.url)];
-		const reply = route && route.reply;
-		if (reply) {
-			let err;
-			let res;
-			if (reply.status >= 400) {
-				err = {
-					status: reply.status,
-					response: reply.result
-				};
-			} else {
-				res = {
-					status: reply.status,
-					body: reply.result
-				};
-			}
+  superagent._patchedBySuperagentMocker = true;
 
-			try {
-				cb && cb(err, res);
-			} catch (e) {
-				console.error('callback error', e.stack || e);
-				const response = new superagent.Response({
-					res: {
-						headers: {},
-						setEncoding: () => {},
-						on: () => {}
-					},
-					req: {
-						method: () => {}
-					},
-					xhr: {
-						responseType: '',
-						getAllResponseHeaders: () => 'a header',
-						getResponseHeader: () => 'a header'
-					}
-				});
-				response.setStatusProperties(e.message);
-				cb && cb(e, response);
-			}
+  // Patch the end method to shallow the request and return the result
+  const reqProto = superagent.Request.prototype;
+  const oldEnd = reqProto.end;
+  reqProto.end = function end(cb) { // do not use function arrow to access to this.url and this.method
+    const route = routesMap[buildRouteKey(this.method, this.url)];
+    const reply = route && route.reply;
+    if (reply) {
+      let err;
+      let res;
+      if (reply.status >= 400) {
+        err = {
+          status: reply.status,
+          response: reply.result
+        };
+      }
+      // else { // sheldon: removed else cuz the correct superagent behavior is to have res even in error case
+        res = {
+          status: reply.status,
+          body: reply.result
+        };
+      // }
 
-			/* TODO: delay: setTimeout(function() {
-				try {
-					cb && cb(null, current(state.request));
-				} catch (ex) {
-					cb && cb(ex, null);
-				}
-			}, value(mock.responseDelay));*/
-		} else {
-			oldEnd.call(this, cb);
-		}
-	};
+      try {
+        cb && cb(err, res);
+      } catch (e) {
+        console.error('callback error', e.stack || e);
+        const response = new superagent.Response({
+          res: {
+            headers: {},
+            setEncoding: () => {},
+            on: () => {}
+          },
+          req: {
+            method: () => {}
+          },
+          xhr: {
+            responseType: '',
+            getAllResponseHeaders: () => 'a header',
+            getResponseHeader: () => 'a header'
+          }
+        });
+        response.setStatusProperties(e.message);
+        cb && cb(e, response);
+      }
 
-	return init;
+      /* TODO: delay: setTimeout(function() {
+       try {
+       cb && cb(null, current(state.request));
+       } catch (ex) {
+       cb && cb(ex, null);
+       }
+       }, value(mock.responseDelay));*/
+    } else {
+      oldEnd.call(this, cb);
+    }
+  };
+
+  return init;
 }
